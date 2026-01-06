186706752dedd63e3b103a43a6798e85ae160eaf
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-06 15:50:01 +0530

    config.py

--- config.py
+++ config.py
@@ -1,16 +1,6 @@
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
-# if not GROQ_API_KEY:
-#     raise ValueError("GROQ_API_KEY not set in .env file")
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-    
+if not GROQ_API_KEY:
+    raise ValueError("❌ GROQ_API_KEY not found. Add it to Streamlit Secrets.")

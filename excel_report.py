e1f31df40c09c1cd0e41c0da3fb1d6c3b9d0138b
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-06 16:42:16 +0530

    excel_report.py

--- excel_report.py
+++ excel_report.py
@@ -1,16 +1,26 @@
+# import pandas as pd
+
+# def generate_excel_report(ap_df, ar_df, cash_df, insights):
+#     path = "finance_report.xlsx"
+
+#     with pd.ExcelWriter(path, engine="openpyxl") as writer:
+#         ap_df.to_excel(writer, "AP", index=False)
+#         ar_df.to_excel(writer, "AR", index=False)
+#         cash_df.to_excel(writer, "Cash", index=False)
+
+#         pd.DataFrame({
+#             "Section": insights.keys(),
+#             "Insight": insights.values()
+#         }).to_excel(writer, "AI Insights", index=False)
+
+#     return path
+
+
+
 import pandas as pd
 
-def generate_excel_report(ap_df, ar_df, cash_df, insights):
+def generate_excel_report(insights):
+    df = pd.DataFrame(insights.items(), columns=["Section", "Insight"])
     path = "finance_report.xlsx"
-
-    with pd.ExcelWriter(path, engine="openpyxl") as writer:
-        ap_df.to_excel(writer, "AP", index=False)
-        ar_df.to_excel(writer, "AR", index=False)
-        cash_df.to_excel(writer, "Cash", index=False)
-
-        pd.DataFrame({
-            "Section": insights.keys(),
-            "Insight": insights.values()
-        }).to_excel(writer, "AI Insights", index=False)
-
+    df.to_excel(path, index=False)
     return path

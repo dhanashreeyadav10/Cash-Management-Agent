41fdde53dab3c4a78f87a44524f5ba22b670cc73
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-06 15:41:58 +0530

    forecasting.py

--- forecasting.py
+++ forecasting.py
@@ -1,17 +1,4 @@
-# import numpy as np
-# from sklearn.linear_model import LinearRegression
 
-# def forecast_series(series, days=30):
-#     series = series.dropna()
-
-#     y = series.values
-#     X = np.arange(len(y)).reshape(-1, 1)
-
-#     model = LinearRegression()
-#     model.fit(X, y)
-
-#     future_X = np.arange(len(y), len(y) + days).reshape(-1, 1)
-#     return model.predict(future_X)
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
@@ -24,3 +11,4 @@ def forecast_series(series, days=30):
 
     future_X = np.arange(len(y), len(y) + days).reshape(-1, 1)
     return model.predict(future_X)
+

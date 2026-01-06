fd9ea5c159d314a9be294cc7b5259d490a70a50f
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-06 16:41:35 +0530

    pdf_report.py

--- pdf_report.py
+++ pdf_report.py
@@ -1,3 +1,22 @@
+# from reportlab.platypus import SimpleDocTemplate, Paragraph
+# from reportlab.lib.styles import getSampleStyleSheet
+
+# def generate_pdf_report(insights):
+#     path = "finance_report.pdf"
+#     styles = getSampleStyleSheet()
+#     doc = SimpleDocTemplate(path)
+
+#     elements = [Paragraph("Finance AI Report", styles["Title"])]
+
+#     for k, v in insights.items():
+#         elements.append(Paragraph(f"<b>{k}</b><br/>{v}", styles["BodyText"]))
+
+#     doc.build(elements)
+#     return path
+
+
+
+
 from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
 
@@ -9,7 +28,8 @@ def generate_pdf_report(insights):
     elements = [Paragraph("Finance AI Report", styles["Title"])]
 
     for k, v in insights.items():
-        elements.append(Paragraph(f"<b>{k}</b><br/>{v}", styles["BodyText"]))
+        elements.append(Paragraph(f"<b>{k}</b>: {v}", styles["BodyText"]))
 
     doc.build(elements)
     return path
+

e71be08f0203d25ac74a9fff453bc09b2f7462fe
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-06 16:40:29 +0530

    file_handler.py

--- file_handler.py
+++ file_handler.py
@@ -1,13 +1,30 @@
+# import pandas as pd
+# from pdf_loader import extract_pdf_text
+
+# def load_uploaded_file(uploaded_file):
+#     name = uploaded_file.name.lower()
+
+#     if name.endswith(".csv"):
+#         return "table", pd.read_csv(uploaded_file)
+
+#     if name.endswith(".pdf"):
+#         return "text", extract_pdf_text(uploaded_file)
+
+#     raise ValueError("Unsupported file format")
+
+
 import pandas as pd
 from pdf_loader import extract_pdf_text
 
-def load_uploaded_file(uploaded_file):
-    name = uploaded_file.name.lower()
+def load_uploaded_file(file):
+    name = file.name.lower()
 
     if name.endswith(".csv"):
-        return "table", pd.read_csv(uploaded_file)
+        return "table", pd.read_csv(file)
 
     if name.endswith(".pdf"):
-        return "text", extract_pdf_text(uploaded_file)
+        return "text", extract_pdf_text(file)
 
-    raise ValueError("Unsupported file format")
+    raise ValueError("Unsupported format")
+
+

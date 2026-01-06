62cbe4b9440dfc2f3ac77590af240481a804c841
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-06 15:41:25 +0530

    file_handler.py

--- file_handler.py
+++ file_handler.py
@@ -1,27 +1,3 @@
-# import pandas as pd
-# from pdf_loader import extract_pdf_text
-# from image_ocr import extract_image_text
-# from data_loader import parse_finance_text_to_df
-
-# def load_uploaded_file(uploaded_file):
-#     name = uploaded_file.name.lower()
-
-#     if name.endswith(".csv"):
-#         return pd.read_csv(uploaded_file)
-
-#     if name.endswith(".xlsx"):
-#         return pd.read_excel(uploaded_file)
-
-#     if name.endswith(".pdf"):
-#         text = extract_pdf_text(uploaded_file)
-#         return parse_finance_text_to_df(text)
-
-#     if name.endswith((".png", ".jpg", ".jpeg")):
-#         text = extract_image_text(uploaded_file)
-#         return parse_finance_text_to_df(text)
-
-#     raise ValueError("Unsupported file type")
-
 
 import pandas as pd
 from pdf_loader import extract_pdf_text
@@ -44,3 +20,4 @@ def load_uploaded_file(uploaded_file):
 
     else:
         raise ValueError("Unsupported file format")
+

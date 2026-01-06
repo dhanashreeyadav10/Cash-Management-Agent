3a02b4ff9d98df07adb076764be6f456f8d26684
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-06 15:48:40 +0530

    llm_client.py

--- llm_client.py
+++ llm_client.py
@@ -1,33 +1,19 @@
-# from groq import Groq
-# from config import GROQ_API_KEY
-
-# client = Groq(api_key=GROQ_API_KEY)
-
-# def call_llm(system_prompt, user_prompt):
-#     try:
-#         response = client.chat.completions.create(
-#             model="llama-3.1-8b-instant",
-#             messages=[
-#                 {"role": "system", "content": system_prompt},
-#                 {"role": "user", "content": user_prompt}
-#             ],
-#             temperature=0.2
-#         )
-#         return response.choices[0].message.content
-#     except Exception as e:
-#         return f"LLM Error: {str(e)}"
 from groq import Groq
 from config import GROQ_API_KEY
 
 client = Groq(api_key=GROQ_API_KEY)
 
 def call_llm(system_prompt, user_prompt):
+    if not GROQ_API_KEY:
+        return "❌ GROQ API key missing. Configure Streamlit Secrets."
+
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_prompt}
         ],
-        temperature=0.2
+        temperature=0.2,
+        max_tokens=800
     )
     return response.choices[0].message.content

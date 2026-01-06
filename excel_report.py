81f7270dcd7123b7608b97b806305571c468b0bf
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-06 16:20:44 +0530

    excel_report.py

--- excel_report.py
+++ excel_report.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
 def generate_excel_report(ap_df, ar_df, cash_df, insights):
-    path = "finance_analysis_report.xlsx"
+    path = "finance_report.xlsx"
 
     with pd.ExcelWriter(path, engine="openpyxl") as writer:
-        ap_df.to_excel(writer, sheet_name="AP", index=False)
-        ar_df.to_excel(writer, sheet_name="AR", index=False)
-        cash_df.to_excel(writer, sheet_name="Cash", index=False)
+        ap_df.to_excel(writer, "AP", index=False)
+        ar_df.to_excel(writer, "AR", index=False)
+        cash_df.to_excel(writer, "Cash", index=False)
 
         pd.DataFrame({
-            "Area": insights.keys(),
-            "Insights": insights.values()
-        }).to_excel(writer, sheet_name="AI Insights", index=False)
+            "Section": insights.keys(),
+            "Insight": insights.values()
+        }).to_excel(writer, "AI Insights", index=False)
 
     return path

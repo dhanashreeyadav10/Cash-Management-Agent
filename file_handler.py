a901ca5080ae30729296fa549ceb036611ead3c9
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-06 16:19:38 +0530

    file_handler.py

--- file_handler.py
+++ file_handler.py
@@ -1,42 +1,13 @@
-
 import pandas as pd
 from pdf_loader import extract_pdf_text
-from image_ocr import extract_image_text
 
 def load_uploaded_file(uploaded_file):
     name = uploaded_file.name.lower()
 
     if name.endswith(".csv"):
-        df = pd.read_csv(uploaded_file)
-        return "table", df
+        return "table", pd.read_csv(uploaded_file)
 
-    elif name.endswith(".pdf"):
-        text = extract_pdf_text(uploaded_file)
-        return "text", text
+    if name.endswith(".pdf"):
+        return "text", extract_pdf_text(uploaded_file)
 
-    elif name.endswith((".png", ".jpg", ".jpeg")):
-        return "text", "⚠ Image OCR is disabled on Streamlit Cloud."
-
-    else:
-        raise ValueError("Unsupported file format")
-
-
-# def load_uploaded_file(uploaded_file):
-#     name = uploaded_file.name.lower()
-
-#     if name.endswith(".csv"):
-#         df = pd.read_csv(uploaded_file)
-#         return "table", df
-
-#     elif name.endswith(".pdf"):
-#         text = extract_pdf_text(uploaded_file)
-#         return "text", text
-
-#     elif name.endswith((".png", ".jpg", ".jpeg")):
-#         text = extract_image_text(uploaded_file)
-#         return "text", text
-
-#     else:
-#         raise ValueError("Unsupported file format")
-
-
+    raise ValueError("Unsupported file format")

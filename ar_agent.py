a63fe3af23bd2ec055726aff551d108003ac1151
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-06 15:34:32 +0530

    ar_agent.py

--- ar_agent.py
+++ ar_agent.py
@@ -1,40 +1,3 @@
-# from forecasting import forecast_series
-# from llm_client import call_llm
-
-# def ar_agent(ar_df):
-#     ar_df["DSO"] = ar_df["days_overdue"] + 30
-#     forecast = forecast_series(ar_df["DSO"])
-
-#     context = f"""
-# Average DSO: {ar_df["DSO"].mean()}
-# Forecasted DSO (next 10 days): {forecast[:10]}
-# """
-
-#     return call_llm(
-#         "You are an Accounts Receivable strategist.",
-#         context + "\nSuggest actions to reduce DSO."
-#     )
-
-
-# from forecasting import forecast_series
-# from llm_client import call_llm
-
-# def ar_agent(ar_df=None, text_context=None):
-#     if ar_df is not None:
-#         ar_df["DSO"] = ar_df["days_overdue"] + 30
-#         forecast = forecast_series(ar_df["DSO"])
-#         context = f"""
-# Average DSO: {ar_df["DSO"].mean()}
-# Forecasted DSO: {forecast[:10]}
-# """
-#     else:
-#         context = f"AR Document Text:\n{text_context[:3000]}"
-
-#     return call_llm(
-#         "You are an Accounts Receivable strategist.",
-#         context + "\nSuggest actions to reduce DSO."
-#     )
-
 
 from forecasting import forecast_series
 from llm_client import call_llm
@@ -47,3 +10,4 @@ def ar_agent(ar_df):
         "You are an AR expert.",
         f"Avg DSO: {ar_df['DSO'].mean()}, Forecast: {forecast[:5]}"
     )
+

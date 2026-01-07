3d1140e89a22586173523b259d9a95a063a4d5c7
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-07 12:24:57 +0530

    dashboard_components.py

--- dashboard_components.py
+++ dashboard_components.py
@@ -1,24 +1,33 @@
+# import streamlit as st
+
+# def metric_card(title, value, status="neutral"):
+#     color = {
+#         "good": "#2ecc71",
+#         "warn": "#f1c40f",
+#         "bad": "#e74c3c",
+#         "neutral": "#bdc3c7"
+#     }[status]
+
+#     st.markdown(
+#         f"""
+#         <div style="
+#             border-left:6px solid {color};
+#             padding:14px;
+#             border-radius:8px;
+#             background:#fafafa;
+#         ">
+#             <b>{title}</b>
+#             <h3>{value}</h3>
+#         </div>
+#         """,
+#         unsafe_allow_html=True
+#     )
+
+
 import streamlit as st
 
-def metric_card(title, value, status="neutral"):
-    color = {
-        "good": "#2ecc71",
-        "warn": "#f1c40f",
-        "bad": "#e74c3c",
-        "neutral": "#bdc3c7"
-    }[status]
-
-    st.markdown(
-        f"""
-        <div style="
-            border-left:6px solid {color};
-            padding:14px;
-            border-radius:8px;
-            background:#fafafa;
-        ">
-            <b>{title}</b>
-            <h3>{value}</h3>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+def metric_card(title, value):
+    if value is None:
+        st.metric(title, "N/A")
+    else:
+        st.metric(title, f"₹ {value:,.0f}")

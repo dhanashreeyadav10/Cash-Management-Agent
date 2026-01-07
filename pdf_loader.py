d77507a3091bd7484c7d7c718c46810bcedd2a34
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-07 12:20:07 +0530

    pdf_loader.py

--- pdf_loader.py
+++ pdf_loader.py
@@ -13,7 +13,9 @@ import pdfplumber
 def extract_pdf_text(file):
     text = ""
     with pdfplumber.open(file) as pdf:
-        for p in pdf.pages:
-            if p.extract_text():
-                text += p.extract_text()
+        for page in pdf.pages:
+            if page.extract_text():
+                text += page.extract_text() + "\n"
     return text
+
+

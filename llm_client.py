dbb4f406e9ed8dd1cc14db3b0392f281cc74ec3f
Dhanashree Yadav <120458692+dhanashreeyadav10@users.noreply.github.com>
2026-01-07 15:28:24 +0530

    llm_client.py

--- llm_client.py
+++ llm_client.py
@@ -1,38 +1,3 @@
-# import requests
-# from config import GROQ_API_KEY
-
-# GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
-
-# def call_llm(system_prompt, user_prompt):
-#     headers = {
-#         "Authorization": f"Bearer {GROQ_API_KEY}",
-#         "Content-Type": "application/json"
-#     }
-
-#     payload = {
-#         "model": "llama-3.1-8b-instant",
-#         "messages": [
-#             {"role": "system", "content": system_prompt},
-#             {"role": "user", "content": user_prompt}
-#         ],
-#         "temperature": 0.2,
-#         "max_tokens": 800
-#     }
-
-#     response = requests.post(
-#         GROQ_API_URL,
-#         headers=headers,
-#         json=payload,
-#         timeout=60
-#     )
-
-#     if response.status_code != 200:
-#         raise RuntimeError(f"GROQ Error {response.status_code}: {response.text}")
-
-#     return response.json()["choices"][0]["message"]["content"]
-
-
-
 import requests
 from config import GROQ_API_KEY
 
